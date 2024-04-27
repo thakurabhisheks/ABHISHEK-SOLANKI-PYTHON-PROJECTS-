cbbc9d28f75a006ba1f96b0383c47bedcafa3217
thakurabhisheks <168290226+thakurabhisheks@users.noreply.github.com>
2024-04-27 21:41:14 +0530

    Create Project5.py

---
+++ Project5.py
@@ -0,0 +1,21 @@
+ # For making Calculator
+first = int(input())
+second = int(input())
+print("1 for add, 2 for sub, 3 for mul, 4 for divide, 5 for power, 6 for float division")
+inp = int(input())
+if inp == 1:
+     print(first+second)
+elif inp == 2:
+     print(first-second)
+elif inp == 3:
+     print(first*second)
+elif inp == 4:
+     print(first/second)
+elif inp == 5:
+     print(first**second)
+elif inp == 6:
+     print(first//second)
+else:
+     print("olease enter only displayed value.")
+ 16 changes: 16 additions & 0 deletions16  
+numberguessing.py
